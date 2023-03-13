2f1cb46610666eff69e6908c0f04a431422d391d
Palleri <Skoog.timmy@gmail.com>
2023-03-13 14:16:22 +0100

    Fixed so that main_DCW not starting when new is added

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -10,41 +10,23 @@ if [ -n "$HOSTNAME" ]; then
         echo $HOSTNAME > /etc/hostname
 fi
 
-if [ -n "$CRON_TIME" ]; then
-    
-    hour=$(echo $CRON_TIME | grep -Po "\d*(?=:)")
-    minute=$(echo $CRON_TIME | grep -Po "(?<=:)\d*")
-    echo -e "\n$minute  $hour   *   *   *   run-parts /etc/periodic/daily" >> /app/root
-
-    else
-
-    echo -e "\n30 12  *   *   *   run-parts /etc/periodic/daily" >> /app/root 
+#if [ -n "$CRON_TIME" ]; then
+#    
+#    hour=$(echo $CRON_TIME | grep -Po "\d*(?=:)")
+#    minute=$(echo $CRON_TIME | grep -Po "(?<=:)\d*")
+#    echo -e "\n$minute  $hour   *   *   *   run-parts /etc/periodic/daily" >> /app/root
+#
+#    else
+#
+#    echo -e "\n30 12  *   *   *   run-parts /etc/periodic/daily" >> /app/root 
+#fi
+if [ -n "$EXCLUDE" ]; then
+    echo $EXCLUDE > /app/EXCLUDE
 fi
-
-if [ "$NOTIFY" = "true" ]; then
-    if [ -n "$NOTIFY_URLS" ]; then
-        echo $NOTIFY_URLS > /app/NOTIFY_URLS
-        echo "Notify activated"
-    fi
-
-    if [ -n "$EXCLUDE" ]; then
-        echo $EXCLUDE > /app/EXCLUDE
-    fi
-
-    if [ "$NOTIFY_DEBUG" = "true" ]; then
-        echo $NOTIFY_DEBUG > /app/NOTIFY_DEBUG
-        echo "NOTIFY DEBUGMODE ACTIVATED"  
-    fi
-fi
-
 cd /app && tar xzvf /app/docker.tgz > /dev/null 2>&1 && cp /app/docker/* /usr/bin/ > /dev/null 2>&1
 chmod +x /app/dockcheck*
 chmod +x /app/regctl
 mv /app/regctl /usr/bin/regctl
+exec /app/dockcheck
 
-if [ -n "$EXCLUDE" ]; then
-    exec /app/dockcheck -e $EXCLUDE
-    else
-    exec /app/dockcheck
-fi
 
